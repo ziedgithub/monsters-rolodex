c9e5103489cb70473e736ff242a09c207d6a6e88
Zied <ziedbenamar6@gmail.com>
2020-04-13 20:37:15 +0200

    fix homepage prop on package.json

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,20 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+src/App.js,1\e\1e43fa63aecd2127be034b3fb9d4caa172275714
+`
+0src/components/card-list/card-list.component.jsx,8\d\8d97cd20c1806e468bd184c73a3853bac8f56066
+;
+src/App.css,a\a\aa7165aefcf8ee3d73539e99ab007daf1aa18d8d
+S
+#src/components/card/card.styles.css,f\c\fc85fb1fe219060c31dc7eab5a4835af975995bb
+_
+/src/components/search-box/search-box.styles.css,e\d\ed5a09a8173c57a0217ede286066ab5989c7db30
+]
+-src/components/card-list/card-list.styles.css,0\2\0204cda2ca67aca522077256e55ec97a8c4a7f12
+V
+&src/components/card/card.component.jsx,8\c\8c46d426419d0b3c08e7af9e03b047fcecb9426c
+b
+2src/components/search-box/search-box.component.jsx,e\b\eb44152fd45faf03fb53e733eef786048b31d38f
--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+
+import './App.css';
+import {SearchBox} from "./components/search-box/search-box.component";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({
+        monsters: users
+      }))
+  }
+
+  updateSearchField = (event) => {
+    this.setState({
+      searchField: event.target.value
+    });
+  }
+
+  render() {
+    const filtredMonsters = this.state.monsters.filter(m => m.name.toLowerCase().includes(this.state.searchField));
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="search monster" handleChange={this.updateSearchField} />
+        <CardList monsters={filtredMonsters} />
+      </div>
+    )
+  }
 }
 
 export default App;
